0b8cf9dc949edc0dc0aa5e544e2a28b98fd6f9dd
Fei <feixia@cs.cmu.edu>
2014-02-01 15:39:31 -0500

    correct formatting

--- src/InvList.java
+++ src/InvList.java
@@ -11,161 +11,174 @@ import org.apache.lucene.search.*;
 
 public class InvList {
 
-  /**
-   * Utility class that makes it easier to deal with postings.
-   */
-  public class DocPosting {
-
-    public int docid = 0;
-    public int tf = 0;
-    public Vector<Integer> positions = new Vector<Integer>();
-
-    /**
-     * Constructor.
-     */
-    public DocPosting(int d, int... locations) {
-      this.docid = d;
-      this.tf = locations.length;
-      for (int i = 0; i < locations.length; i++)
-        this.positions.add(locations[i]);
-    }
-
-    /**
-     *  Inserts the postings from dp2 into this (dp1) docPosting.
-     *  Maintains a sorted order.
-     */
-    public void insert(DocPosting dp2) {
-
-      int dp1Pos = 0; /* Offset to the current position in dp1 */
-      int dp2Pos = 0; /* Offset to the current position in dp2 */
-
-      // Each pass of the loop inserts one position from dp2 into dp1.
-      while (dp2Pos < dp2.tf) {
-
-        // Skip positions in dp1 that are less than the current position in dp2.
-        while ((dp1Pos < this.tf)
-            && (this.positions.elementAt(dp1Pos) < dp2.positions.elementAt(dp2Pos))) {
-          dp1Pos++;
-        }
-
-	/*
-         *  dp1Pos points to where a dp2 position can be inserted
-	 *  (possibly at the end of the list).  If it is not a
-	 *  duplicate, insert it.
+	/**
+	 * Utility class that makes it easier to deal with postings.
 	 */
-        if ((dp1Pos == this.tf)
-            || (this.positions.elementAt(dp1Pos) > dp2.positions.elementAt(dp2Pos))) {
+	public class DocPosting {
 
-          this.positions.add(dp1Pos, dp2.positions.elementAt(dp2Pos));
-          this.tf++;
-        }
+		public int docid = 0;
+		public int tf = 0;
+		public Vector<Integer> positions = new Vector<Integer>();
 
-        dp1Pos++;
-        dp2Pos++;
-      }
-    }
-  }
+		/**
+		 * Constructor.
+		 */
+		public DocPosting(int d, int... locations) {
+			this.docid = d;
+			this.tf = locations.length;
+			for (int i = 0; i < locations.length; i++)
+				this.positions.add(locations[i]);
+		}
 
-  /**
-   * Class variables.
-   */
-  public int ctf = 0;
-  public int df = 0;
-  public Vector<DocPosting> postings = new Vector<DocPosting>();
+		/**
+		 * Inserts the postings from dp2 into this (dp1) docPosting. Maintains a
+		 * sorted order.
+		 */
+		public void insert(DocPosting dp2) {
 
-  /**
-   * An empty inverted list. Useful for some query operators.
-   */
-  public InvList() {
-  }
+			int dp1Pos = 0; /* Offset to the current position in dp1 */
+			int dp2Pos = 0; /* Offset to the current position in dp2 */
 
-  /**
-   * Fetch the inverted list from the index.
-   */
-  public InvList(String termString, String fieldString) throws IOException {
+			// Each pass of the loop inserts one position from dp2 into dp1.
+			while (dp2Pos < dp2.tf) {
 
-    // Prepare to access the index.
-    BytesRef termBytes = new BytesRef(termString);
-    Term term = new Term(fieldString, termBytes);
+				// Skip positions in dp1 that are less than the current position
+				// in dp2.
+				while ((dp1Pos < this.tf)
+						&& (this.positions.elementAt(dp1Pos) < dp2.positions
+								.elementAt(dp2Pos))) {
+					dp1Pos++;
+				}
 
-    if (QryEval.READER.docFreq(term) < 1)
-      return;
+				/*
+				 * dp1Pos points to where a dp2 position can be inserted
+				 * (possibly at the end of the list). If it is not a duplicate,
+				 * insert it.
+				 */
+				if ((dp1Pos == this.tf)
+						|| (this.positions.elementAt(dp1Pos) > dp2.positions
+								.elementAt(dp2Pos))) {
 
-    // Lookup the inverted list.
-    DocsAndPositionsEnum iList = MultiFields.getTermPositionsEnum(QryEval.READER,
-        MultiFields.getLiveDocs(QryEval.READER), fieldString, termBytes);
+					this.positions.add(dp1Pos, dp2.positions.elementAt(dp2Pos));
+					this.tf++;
+				}
 
-    // Copy from Lucene inverted list format to our inverted list format. This is a little
-    // inefficient, but allows query operators such as #syn, #od/n, and #uw/n to be insulated from
-    // the details of Lucene inverted list implementations.
-    while (iList.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) {
+				dp1Pos++;
+				dp2Pos++;
+			}
+		}
+	}
 
-      int tf = iList.freq();
-      int[] pos = new int[tf];
+	/**
+	 * Class variables.
+	 */
+	public int ctf = 0;
+	public int df = 0;
+	public Vector<DocPosting> postings = new Vector<DocPosting>();
 
-      for (int j = 0; j < tf; j++)
-        pos[j] = iList.nextPosition();
+	/**
+	 * An empty inverted list. Useful for some query operators.
+	 */
+	public InvList() {
+	}
 
-      this.postings.add(new DocPosting(iList.docID(), pos));
-      this.df++;
-      this.ctf += tf;
-    }
-  }
+	/**
+	 * Fetch the inverted list from the index.
+	 */
+	public InvList(String termString, String fieldString) throws IOException {
 
-  /**
-   * Inserts inverted list i2 into this inverted list (called "i1"). Maintains a sorted order.
-   */
-  public void insert(InvList i2) {
+		// Prepare to access the index.
+		BytesRef termBytes = new BytesRef(termString);
+		Term term = new Term(fieldString, termBytes);
 
-    int i1Doc = 0; /* Offset to the current document in i1 */
-    int i2Doc = 0; /* Offset to the current document in i2 */
+		if (QryEval.READER.docFreq(term) < 1)
+			return;
 
-    // Each pass of the loop inserts one docPosting from i2 into i1.
-    while (i2Doc < i2.df) {
+		// Lookup the inverted list.
+		DocsAndPositionsEnum iList = MultiFields.getTermPositionsEnum(
+				QryEval.READER, MultiFields.getLiveDocs(QryEval.READER),
+				fieldString, termBytes);
 
-      // Skip documents in i1 that are less than the current document in i2.
-      while ((i1Doc < this.df)
-          && (this.postings.elementAt(i1Doc).docid < i2.postings.elementAt(i2Doc).docid)) {
-        i1Doc++;
-      }
+		// Copy from Lucene inverted list format to our inverted list format.
+		// This is a little
+		// inefficient, but allows query operators such as #syn, #od/n, and
+		// #uw/n to be insulated from
+		// the details of Lucene inverted list implementations.
+		while (iList.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) {
 
-      // i1Doc points to where an i2 document can be merged or inserted (possibly at the end of the
-      // list).
-      if ((i1Doc < this.df)
-          && (this.postings.elementAt(i1Doc).docid == i2.postings.elementAt(i2Doc).docid)) {
+			int tf = iList.freq();
+			int[] pos = new int[tf];
 
-        /* Merge the i1 and i2 postings for this document */
-        this.postings.elementAt(i1Doc).insert(i2.postings.elementAt(i2Doc));
-        this.ctf += i2.postings.elementAt(i2Doc).tf;
-      } else {
+			for (int j = 0; j < tf; j++)
+				pos[j] = iList.nextPosition();
 
-        /* Insert the i2 document into i1 */
-        this.postings.add(i1Doc, i2.postings.elementAt(i2Doc));
-        this.ctf += i2.postings.elementAt(i2Doc).tf;
-        this.df++;
-      }
+			this.postings.add(new DocPosting(iList.docID(), pos));
+			this.df++;
+			this.ctf += tf;
+		}
+	}
 
-      i1Doc++;
-      i2Doc++;
-    }
-  }
+	/**
+	 * Inserts inverted list i2 into this inverted list (called "i1"). Maintains
+	 * a sorted order.
+	 */
+	public void insert(InvList i2) {
 
-  /**
-   * Handy for debugging.
-   */
-  public void print() {
+		int i1Doc = 0; /* Offset to the current document in i1 */
+		int i2Doc = 0; /* Offset to the current document in i2 */
 
-    System.out.println("df:  " + this.df + ", ctf: " + this.ctf);
+		// Each pass of the loop inserts one docPosting from i2 into i1.
+		while (i2Doc < i2.df) {
 
-    for (int i = 0; i < this.df; i++) {
-      System.out.print("docid:  " + this.postings.elementAt(i).docid + ", tf: "
-          + this.postings.elementAt(i).tf + ", locs: ");
+			// Skip documents in i1 that are less than the current document in
+			// i2.
+			while ((i1Doc < this.df)
+					&& (this.postings.elementAt(i1Doc).docid < i2.postings
+							.elementAt(i2Doc).docid)) {
+				i1Doc++;
+			}
 
-      for (int j = 0; j < this.postings.elementAt(i).tf; j++) {
-        System.out.print(this.postings.elementAt(i).positions.elementAt(j) + " ");
-      }
+			// i1Doc points to where an i2 document can be merged or inserted
+			// (possibly at the end of the
+			// list).
+			if ((i1Doc < this.df)
+					&& (this.postings.elementAt(i1Doc).docid == i2.postings
+							.elementAt(i2Doc).docid)) {
 
-      System.out.println();
-    }
-  }
+				/* Merge the i1 and i2 postings for this document */
+				this.postings.elementAt(i1Doc).insert(
+						i2.postings.elementAt(i2Doc));
+				this.ctf += i2.postings.elementAt(i2Doc).tf;
+			} else {
+
+				/* Insert the i2 document into i1 */
+				this.postings.add(i1Doc, i2.postings.elementAt(i2Doc));
+				this.ctf += i2.postings.elementAt(i2Doc).tf;
+				this.df++;
+			}
+
+			i1Doc++;
+			i2Doc++;
+		}
+	}
+
+	/**
+	 * Handy for debugging.
+	 */
+	public void print() {
+
+		System.out.println("df:  " + this.df + ", ctf: " + this.ctf);
+
+		for (int i = 0; i < this.df; i++) {
+			System.out.print("docid:  " + this.postings.elementAt(i).docid
+					+ ", tf: " + this.postings.elementAt(i).tf + ", locs: ");
+
+			for (int j = 0; j < this.postings.elementAt(i).tf; j++) {
+				System.out.print(this.postings.elementAt(i).positions
+						.elementAt(j) + " ");
+			}
+
+			System.out.println();
+		}
+	}
 }
