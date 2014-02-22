bc826b434c85803ea517368edd9ae357700080fc
Fei <feixia@cs.cmu.edu>
2014-02-21 23:54:00 -0500

    add QryopUW.java

---
+++ src/QryopUW.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+
+public class QryopUW {
+	private int dist;
+	
+	public QryopUW(int dist, Qryop... q) {
+		
+	}
+	
+	public QryopUW(int dist) {
+
+	}
+	
+	public void add(Qryop a) {
+		
+	}
+	
+	public QryResult evaluate() throws IOException {
+		
+	}
+}
+
